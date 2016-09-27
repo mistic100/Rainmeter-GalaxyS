a4f6cfbfc7e787843aea03f0e36052d37b128255
mistic100 <contact@git.strangeplanet.fr>
2016-09-27 19:13:18 +0200

    Add sub-skin with minimal vu-meter

--- ToggleClock Script/ToggleClock.bat
+++ ToggleClock Script/ToggleClock.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Rainmeter\Rainmeter.exe" !ToggleMeterGroup FullClock GalaxyS
+"C:\Program Files\Rainmeter\Rainmeter.exe" !ToggleMeterGroup FullClock GalaxyS\Main
+"C:\Program Files\Rainmeter\Rainmeter.exe" !Toggle GalaxyS\VUMeter
